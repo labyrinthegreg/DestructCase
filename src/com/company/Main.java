289daf4df2d70a4ea8b335b4a0c01d2c4ffec598
Léo G <leo.gregori95@gmail.com>
2020-10-14 10:20:22 +0200

    Organisation des fonctions dans le Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        //Déclaration des variables
         String pseudoJ1;
         String pseudoJ2;
+
+        //Menu puis demande des pseudos une fois la demande de commencer la partie
         FonctionDestructCase.afficherMenu();
         pseudoJ1 = FonctionDestructCase.demandePseudo1();
         System.out.println("Joueur1: " + pseudoJ1);
         pseudoJ2 = FonctionDestructCase.demandePseudo2();
         System.out.println("Joueur2: " + pseudoJ2);
+
+
     }
 }
